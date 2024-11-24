08efbb69aa6747595ca07b67486e8c68d88bb013
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2024-11-24 00:51:56 -0300

    subiendo clase user y userController

---
+++ src/main/java/proyecto/odontologia/webapp/springboot_web/controller/UserController.java
@@ -0,0 +1,18 @@
+package proyecto.odontologia.webapp.springboot_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class UserController {
+
+    @GetMapping("index")
+    public String index(Model model ) {
+        model.addAttribute("titulo", "hola mundo");
+        return "index";
+    }
+    
+    
+}
