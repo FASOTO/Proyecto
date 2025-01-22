7d9edd2fadc2c45bf4cd843e72ace39ecbfe6004
CeciliaMon <dreamynylle17@gmail.com>
2025-01-22 20:33:14 -0300

    Estilos y organización de columnas de formPaciente.html cambiados, estilos para la tabla de paciente.html agregada y solapas de informaciónPaciente.html agregadas

---
+++ src/main/resources/static/js/declaracion-jurada.js
@@ -0,0 +1,328 @@
+/*---------------Hemanos sano----------------*/
+
+document.getElementById('hermanosSi').addEventListener('change', function() {
+    document.getElementById('hermanosSanosField').style.display = 'block';
+});
+
+document.getElementById('hermanosSi').addEventListener('change', function() { 
+    const field = document.getElementById('hermanosSanosField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('hermanosNo').addEventListener('change', function() {
+    document.getElementById('hermanosSanosField').style.display = 'none';
+});
+
+/*---------------Enfermedad Tipo----------------*/
+
+document.getElementById('enfermedadSi').addEventListener('change', function() {
+    document.getElementById('enfermedadTipoField').style.display = 'block';
+});
+document.getElementById('enfermedadSi').addEventListener('change', function() { 
+    const field = document.getElementById('enfermedadTipoField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('enfermedadNo').addEventListener('change', function() {
+    document.getElementById('enfermedadTipoField').style.display = 'none';
+});
+
+/*---------------Deporte Malestar----------------*/
+
+document.getElementById('deporteSi').addEventListener('change', function() {
+    document.getElementById('deporteMalestarField').style.display = 'block';
+});
+
+document.getElementById('deporteSi').addEventListener('change', function() { 
+    const field = document.getElementById('deporteMalestarField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('deporteNo').addEventListener('change', function() {
+    document.getElementById('deporteMalestarField').style.display = 'none';
+});
+
+/*---------------Droga Alergia----------------*/
+
+document.getElementById('drogaAlergiaSi').addEventListener('change', function() {
+    document.getElementById('drogaAlergiaTipoField').style.display = 'block';
+});
+
+document.getElementById('drogaAlergiaSi').addEventListener('change', function() { 
+    const field = document.getElementById('drogaAlergiaTipoField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('drogaAlergiaNo').addEventListener('change', function() {
+    document.getElementById('drogaAlergiaTipoField').style.display = 'none';
+});
+
+/*---------------Fiebre Reumática Medicación----------------*/
+
+document.getElementById('fiebreReumaticaSi').addEventListener('change', function() {
+    document.getElementById('fiebreMedicacionField').style.display = 'block';
+});
+
+document.getElementById('fiebreReumaticaSi').addEventListener('change', function() { 
+    const field = document.getElementById('fiebreMedicacionField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('fiebreReumaticaNo').addEventListener('change', function() {
+    document.getElementById('fiebreMedicacionField').style.display = 'none';
+});
+
+/*---------------Diabetes Control----------------*/
+
+document.getElementById('diabeticoSi').addEventListener('change', function() {
+    document.getElementById('diabetesControlField').style.display = 'block';
+});
+
+document.getElementById('diabeticoSi').addEventListener('change', function() { 
+    const field = document.getElementById('diabetesControlField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('diabeticoNo').addEventListener('change', function() {
+    document.getElementById('diabetesControlField').style.display = 'none';
+});
+
+/*---------------Cardiaco Tipo----------------*/
+
+document.getElementById('cardiacoSi').addEventListener('change', function() {
+    document.getElementById('cardiacoTipoField').style.display = 'block';
+});
+
+document.getElementById('cardiacoSi').addEventListener('change', function() { 
+    const field = document.getElementById('cardiacoTipoField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('cardiacoNo').addEventListener('change', function() {
+    document.getElementById('cardiacoTipoField').style.display = 'none';
+});
+
+/*---------------Aspirina o anticoagulante Frecuencia----------------*/
+
+document.getElementById('aspirinaSi').addEventListener('change', function() {
+    document.getElementById('aspirinaFrecuenciaField').style.display = 'block';
+});
+
+document.getElementById('aspirinaSi').addEventListener('change', function() { 
+    const field = document.getElementById('aspirinaFrecuenciaField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('anticoagulanteSi').addEventListener('change', function() {
+    document.getElementById('aspirinaFrecuenciaField').style.display = 'block';
+});
+
+document.getElementById('anticoagulanteSi').addEventListener('change', function() { 
+    const field = document.getElementById('aspirinaFrecuenciaField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('ninguna').addEventListener('change', function() {
+    document.getElementById('aspirinaFrecuenciaField').style.display = 'none';
+});
+
+/*---------------Chagas Tratamiento----------------*/
+
+document.getElementById('chagasSi').addEventListener('change', function() {
+    document.getElementById('chagasTratamientoField').style.display = 'block';
+});
+
+document.getElementById('chagasSi').addEventListener('change', function() { 
+    const field = document.getElementById('chagasTratamientoField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('chagasNo').addEventListener('change', function() {
+    document.getElementById('chagasTratamientoField').style.display = 'none';
+});
+
+/*---------------Hepatitis Tipo----------------*/
+
+document.getElementById('hepatitisSi').addEventListener('change', function() {
+    document.getElementById('hepatitisTipoField').style.display = 'block';
+});
+
+document.getElementById('hepatitisSi').addEventListener('change', function() { 
+    const field = document.getElementById('hepatitisTipoField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('hepatitisNo').addEventListener('change', function() {
+    document.getElementById('hepatitisTipoField').style.display = 'none';
+});
+
+/*---------------Hepatico Tipo----------------*/
+
+document.getElementById('hepaticoSi').addEventListener('change', function() {
+    document.getElementById('hepaticoTipoField').style.display = 'block';
+});
+
+document.getElementById('hepaticoSi').addEventListener('change', function() { 
+    const field = document.getElementById('hepaticoTipoField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('hepaticoNo').addEventListener('change', function() {
+    document.getElementById('hepaticoTipoField').style.display = 'none';
+});
+
+/*---------------Epileptico Medicacion----------------*/
+
+document.getElementById('epilepticoSi').addEventListener('change', function() {
+    document.getElementById('epilepticoMedicacionField').style.display = 'block';
+});
+
+document.getElementById('epilepticoSi').addEventListener('change', function() { 
+    const field = document.getElementById('epilepticoMedicacionField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('epilepticoNo').addEventListener('change', function() {
+    document.getElementById('epilepticoMedicacionField').style.display = 'none';
+});
+
+/*---------------Enfermedad Infecto tipo----------------*/
+
+document.getElementById('infectoSi').addEventListener('change', function() {
+    document.getElementById('infectoField').style.display = 'block';
+});
+
+document.getElementById('infectoSi').addEventListener('change', function() { 
+    const field = document.getElementById('infectoField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('infectoNo').addEventListener('change', function() {
+    document.getElementById('infectoField').style.display = 'none';
+});
+
+/*--------------- Operado Field----------------*/
+
+document.getElementById('operadoSi').addEventListener('change', function() {
+    document.getElementById('operadoField').style.display = 'block';
+});
+
+document.getElementById('operadoSi').addEventListener('change', function() { 
+    const field = document.getElementById('operadoField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('operadoNo').addEventListener('change', function() {
+    document.getElementById('operadoField').style.display = 'none';
+});
+
+/*--------------- Respiratorio Field----------------*/
+
+document.getElementById('respiratorioSi').addEventListener('change', function() {
+    document.getElementById('respiratorioField').style.display = 'block';
+});
+
+document.getElementById('respiratorioSi').addEventListener('change', function() { 
+    const field = document.getElementById('respiratorioField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('respiratorioNo').addEventListener('change', function() {
+    document.getElementById('respiratorioField').style.display = 'none';
+});
+
+/*--------------- embarazada Field----------------*/
+
+document.getElementById('embarazadaSi').addEventListener('change', function() {
+    document.getElementById('embarazadaField').style.display = 'block';
+});
+
+document.getElementById('embarazadaSi').addEventListener('change', function() { 
+    const field = document.getElementById('embarazadaField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('embarazadaNo').addEventListener('change', function() {
+    document.getElementById('embarazadaField').style.display = 'none';
+});
+
+/*--------------- Recomendacion Medica Field----------------*/
+
+document.getElementById('recomendacionMedicaSi').addEventListener('change', function() {
+    document.getElementById('recomendacionMedicaField').style.display = 'block';
+});
+
+document.getElementById('recomendacionMedicaSi').addEventListener('change', function() { 
+    const field = document.getElementById('recomendacionMedicaField'); 
+    field.style.display = 'block'; 
+    field.classList.add('highlight'); 
+    setTimeout(() => { 
+        field.classList.remove('highlight'); }, 800); // Tiempo en milisegundos para que el color se desvanezca 
+});
+
+document.getElementById('recomendacionMedicaNo').addEventListener('change', function() {
+    document.getElementById('recomendacionMedicaField').style.display = 'none';
+});
+
+/*--------------- Boton Siguiente -----------------------*/
+function nextTab(target) {
+    const tabTrigger = new bootstrap.Tab(document.querySelector(`[data-bs-target="${target}"]`));
+    tabTrigger.show();
+    // Desplazarse al inicio de la página
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
